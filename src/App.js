08de74158c69d71d5baa5b37b03bbd2eb81010dc
Siong Leong <shaunleong@outlook.com>
2020-06-14 22:54:37 +0800

    Created modular articles list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ArticlesList from './pages/ArticlesList';
-import ArticlePage from './pages/ArticlePage';
-import NavBar from './NavBar';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -19,7 +16,7 @@ class App extends Component {
           <div id="page-body">
             <Route path="/" component={HomePage} exact />
             <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesList} />
+            <Route path="/articles-list" component={ArticlesListPage} />
             <Route path="/article/:name" component={ArticlePage} />
           </div>
         </div>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import articleContent from './article-content';
+import React from "react";
+import articleContent from "./article-content";
+import ArticlesList from "../components/ArticlesList";
 
 const ArticlePage = ({ match }) => {
-    const name = match.params.name;
-    const article = articleContent.find(article => article.name === name);
+  const name = match.params.name;
+  const article = articleContent.find((article) => article.name === name);
 
-    if (!article) return <h1>Article does not exist!</h1>
+  if (!article) return <h1>Article does not exist!</h1>;
 
-    return (
-        <>
-        <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        </>
-    );
-}
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
+    </>
+  );
+};
 
 export default ArticlePage;
