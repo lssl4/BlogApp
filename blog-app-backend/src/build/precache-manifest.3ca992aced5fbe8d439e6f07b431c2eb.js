8bb67d1d7154e5816df351bda4d92e517b5b6a56
Siong Leong <shaunleong@outlook.com>
2020-06-15 21:11:25 +0800

    Built project and transferred to back end. Ready for release

---
+++ blog-app-backend/src/build/precache-manifest.3ca992aced5fbe8d439e6f07b431c2eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300245acc0216c00097ff2ce16d35bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "450f4cb119aa13732fb8",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "4bc7ada51f1eee6d5ac5",
+    "url": "/static/js/2.8b109ba5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8b109ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450f4cb119aa13732fb8",
+    "url": "/static/js/main.750bef71.chunk.js"
+  },
+  {
+    "revision": "b4651f690cc7ea3f34de",
+    "url": "/static/js/runtime-main.0a8a2a2e.js"
+  }
+]);
