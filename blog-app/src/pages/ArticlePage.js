8f726b98dcc7bf02c88721866eae6faadfb1d81a
Siong Leong <shaunleong@outlook.com>
2020-06-15 16:04:57 +0800

    Updated Article page with upvotes data

--- blog-app/src/pages/ArticlePage.js
+++ blog-app/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
@@ -6,6 +6,18 @@ import NotFoundPage from "./NotFoundPage";
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find((article) => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
 
   if (!article) return <NotFoundPage></NotFoundPage>;
 
@@ -16,6 +28,8 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
+
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
