b57f5011d8107aa05c8fde68e677f611864749a9
Siong Leong <shaunleong@outlook.com>
2020-06-15 16:35:59 +0800

    Added comments list and upvotes section with button

--- blog-app/src/pages/ArticlePage.js
+++ blog-app/src/pages/ArticlePage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
+import UpvotesSection from "../components/UpvotesSection";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
@@ -28,11 +30,17 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
-      <p>This post has been upvoted {articleInfo.upvotes} times</p>
+      <UpvotesSection
+        articleName={name}
+        upvotes={articleInfo.upvotes}
+        setArticleInfo={setArticleInfo}
+      />
 
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
+
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </>
